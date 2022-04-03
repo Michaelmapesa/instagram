9b52fd8b757b03954fdbd7f90b228f8933687c7f
michael mapesa <michaelmapesa93@gmail.com>
2022-04-03 07:39:32 +0300

    added a view function, home images

--- myinstagram/views.py
+++ myinstagram/views.py
@@ -1,3 +1,92 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Image,Location,tags, Profile, Review, NewsLetterRecipients, Like
+from django.http  import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+from .forms import NewImageForm, UpdatebioForm, ReviewForm
+from .email import send_welcome_email
+from .forms import NewsLetterForm
 
-# Create your views here.
+# Views
+tags = tags.objects.all()
+
+@login_required(login_url='/accounts/login/')
+def home_images(request):
+    # Display all images here:
+
+    # images = Image.objects.all()
+
+    locations = Location.objects.all()
+
+    if request.GET.get('location'):
+        pictures = Image.filter_by_location(request.GET.get('location'))
+
+    elif request.GET.get('tags'):
+        pictures = Image.filter_by_tag(request.GET.get('tags'))
+
+    elif request.GET.get('search_term'):
+        pictures = Image.search_image(request.GET.get('search_term'))
+
+    else:
+        pictures = Image.objects.all()
+
+    form = NewsLetterForm
+
+
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST or None)
+        if form.is_valid():
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+
+            recipient = NewsLetterRecipients(name=name, email=email)
+            recipient.save()
+            send_welcome_email(name, email)
+
+            HttpResponseRedirect('home_images')
+
+    return render(request, 'index.html', {'locations':locations,
+                                          'tags': tags,
+                                          'pictures':pictures, 'letterForm':form})
+
+def image(request, id):
+
+    try:
+        image = Image.objects.get(pk = id)
+
+    except DoesNotExist:
+        raise Http404()
+
+    current_user = request.user
+    comments = Review.get_comment(Review, id)
+
+    #
+    # p = Image.objects.get(image_id=id)
+    # onelike = Like.objects.get_or_create(user=request.user, image_id=id)
+    # likes = p.like_set.all().count()
+
+
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            comment = form.cleaned_data['comment']
+
+            review = Review()
+            review.image = image
+            review.user = current_user
+            review.comment = comment
+            review.save()
+
+    else:
+        form = ReviewForm()
+
+
+        # return HttpResponseRedirect(reverse('image', args=(image.id,)))
+
+    return render(request, 'image.html', {"image": image,
+                                          'form':form,
+                                          'comments':comments,
+                                          })
+
