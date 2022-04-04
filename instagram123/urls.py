de320abc476dd666bd59946a9b04d7e035d1a99f
michael mapesa <michaelmapesa93@gmail.com>
2022-04-04 07:37:07 +0300

    modify url to debug login

--- instagram123/urls.py
+++ instagram123/urls.py
@@ -18,10 +18,10 @@ from django.urls import path,re_path,include
 from django.contrib.auth import views
 
 urlpatterns = [
+    re_path(r'^accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('',include('myinstagram.urls')),
     re_path(r'^accounts/', include('django_registration.backends.one_step.urls')),
-    re_path(r'^accounts/', include('django.contrib.auth.urls')),
     # re_path(r'^logout/$', views.logout, {"next_page": '/'}),
 
 
