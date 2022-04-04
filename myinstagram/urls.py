50500beed145e27b34c47568d1094d9f924c1280
michael mapesa <michaelmapesa93@gmail.com>
2022-04-04 08:25:49 +0300

    modify static

--- myinstagram/urls.py
+++ myinstagram/urls.py
@@ -5,13 +5,13 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     re_path(r'^$',views.home_images,name='homePage'),
-    re_path(r'^search/', views.search_users, name='search_users'),
+    path('^search/', views.search_users, name='search_users'),
     re_path(r'^image/(\d+)',views.image,name ='image'),
-    re_path(r'^users/', views.user_list, name = 'user_list'),
-    re_path(r'^new/image$', views.new_image, name='new_image'),
-    re_path(r'^edit/profile$', views.edit_profile, name='edit_profile'),
-    re_path(r'^profile/(?P<username>[0-9]+)$', views.individual_profile_page, name='individual_profile_page'),
-    re_path(r'^myprofile/$', views.myprofile, name='myprofile'),
+    path('^users/', views.user_list, name = 'user_list'),
+    path('^new/image$', views.new_image, name='new_image'),
+    path('^edit/profile$', views.edit_profile, name='edit_profile'),
+    path('^profile/(?P<username>[0-9]+)$', views.individual_profile_page, name='individual_profile_page'),
+    path('^myprofile/$', views.myprofile, name='myprofile'),
 
     
 ]
